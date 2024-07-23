46ee33672b0abcb8931a92103d44abd4afc9e607
Michal Lewandowski <michal.lewandowski1988@gmail.com>
2024-07-23 15:17:24 +0200

    move to case for string check

--- check-version.sh
+++ check-version.sh
@@ -3,11 +3,13 @@
 # Function to check if running on Arch base distro
 check_distro() {
   if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    if [[ "$ID" = "arch" || "$ID_LIKE" = "arch" ]]; then
+  . /etc/os-release
+  case $ID in
+    !arch | !endeavouros)
       echo "Error: This script must be run on Arch linux base distro. Current OS: $ID" >&2
       return 1
-    fi
+      ;;
+  esac
   else
     echo "Error: Unable to determine OS. /etc/os-release file not found." >&2
     return 1
