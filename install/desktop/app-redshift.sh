b976031031a9bd6af79caa929014d4f1fbe53b1d
Michal Lewandowski <michal.lewandowski1988@gmail.com>
2024-07-24 09:46:58 +0200

    disable launching redshift

--- install/desktop/app-redshift.sh
+++ install/desktop/app-redshift.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.config/redshift
 mkdir -p ~/.config/autostart
 cp ~/.local/share/omakarch/configs/redshift.conf ~/.config/redshift/
 cp ~/.local/share/omakarch/configs/Redshift.desktop ~/.config/autostart/Redshift.desktop
-gtk-launch Redshift.desktop >/dev/null 2>&1
+# gtk-launch Redshift.desktop >/dev/null 2>&1
