22f09718bc6e10cf2a15fa02a053aaba74ee2736
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2024-08-12 09:37:11 +0200

    fix problem with vscode configuration; add option to open terminal in file dir

--- install/desktop/app-vscode.sh
+++ install/desktop/app-vscode.sh
@@ -1,7 +1,8 @@
 sudo pacman -S --noconfirm code
 
 mkdir -p ~/.config/Code/User
-cp ~/.local/share/omakarch/configs/vscode.json ~/.config/Code/User/settings.json
+cp ~/.local/share/omakarch/configs/vscode.json ~/.config/Code\ -\ OSS/User/settings.json
+cp ~/.local/share/omakarch/configs/vscode-keybindings.json ~/.config/Code\ -\ OSS/User/keybindings.json
 
 # Install default supported themes
 code --install-extension enkia.tokyo-night
