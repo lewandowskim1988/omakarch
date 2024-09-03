5fd77b9b2d5a5ef6f24cbdefa542937074bfaacf
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2024-09-03 10:58:32 +0200

    replace alias for podman with symlink

--- install/terminal/devops-tools.sh
+++ install/terminal/devops-tools.sh
@@ -17,6 +17,10 @@ yay --answerclean None --answerdiff None --removemake --cleanafter --noconfirm -
   kubectl-view-secret-bin \
   apache-tools
 
+# Configuration for minikube over KVM
 sudo usermod -a -G libvirt $(whoami)
 sudo systemctl start libvirtd.service
 sudo systemctl enable libvirtd.service
+
+# Make symlink for podman/docker since some applications like make are not able to read alias
+ln -sf /usr/bin/podman /usr/local/bin/docker
