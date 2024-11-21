5062b05471cae54ba5ed1b3570d8fde730affd20
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2024-11-21 10:59:22 +0100

    fix alias; use fzf for theme; open port for localsend

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -1,1 +1,5 @@
 yay --answerclean None --answerdiff None --removemake --cleanafter --noconfirm -S localsend-bin
+
+# Open localsend port so other devices from network can connect
+firewall-cmd --permanent --zone=public --add-port=53317/tcp
+firewall-cmd --permanent --zone=public --add-port=53317/udp
