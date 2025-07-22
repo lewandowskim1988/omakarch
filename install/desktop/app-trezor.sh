a66b3fd3281ce6d1102afb294761513bfa9d30d4
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2025-07-22 09:02:56 +0200

    update trezor package

--- install/desktop/app-trezor.sh
+++ install/desktop/app-trezor.sh
@@ -1,1 +1,1 @@
-yay --answerclean None --answerdiff None --removemake --cleanafter --noconfirm -S trezor-suite-appimage
+yay --answerclean None --answerdiff None --removemake --cleanafter --noconfirm -S trezor-suite-bin
