a4ed3a7ce9915869f0b63e1657201044bafea049
Michał Lewandowski <michal.lewandowski1988@gmail.com>
2025-08-06 13:38:09 +0200

    fix vscode theme update

--- themes/set-vscode-theme.sh
+++ themes/set-vscode-theme.sh
@@ -1,4 +1,4 @@
 if command -v code &>/dev/null; then
   code --install-extension $VSC_EXTENSION >/dev/null
-  sed -i "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"$VSC_THEME\"/g" ~/.config/Code/User/settings.json
+  sed -i "s/\"workbench.colorTheme\": \".*\"/\"workbench.colorTheme\": \"$VSC_THEME\"/g" ~/.config/Code\ -\ OSS/User/settings.json
 fi
